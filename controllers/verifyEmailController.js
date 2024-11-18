b1a90c9b952c06327bc2e975de7caf04a5b1acee
Williams Abiola <williams.abiola@itech.ng>
2024-11-18 13:18:41 +0100

    Corrected Errors

--- controllers/verifyEmailController.js
+++ controllers/verifyEmailController.js
@@ -1,55 +1,11 @@
 // controllers/verifyEmailController.js
 
-const { Shareholder } = require('../models'); // Import Sequelize models
-const generateCode = require('../utils/codeGenerator');
-const sendEmail = require('../utils/emailSender');
-const winston = require('winston'); // Import winston for logging
 
 const verifyEmail = async (req, res) => {
     const { mail } = req.body;
 
-    // Validate input
-    if (!mail) {
-        return res.status(400).json({ error: 'Email is required' });
-    }
-
-    try { 
-        const shareholder = await Shareholder.findOne({
-            where: { mail: mail },
-            attributes: ['mobile'] // Only fetch the mobile number
-        });
-    
-        if (shareholder) {
-            const mobile = shareholder.mobile;
-            const code = generateCode();  // Generate verification code
-    
-            // Send email with the generated code
-            const mailSent = await sendEmail(mail, code);
-    
-            if (mailSent) {
-                return res.json({ code, exists: true, mobile });
-            } else {
-                winston.error(`Mail sending failed for email: ${mail}`);
-                return res.status(500).json({ error: 'Failed to send mail. Please try again later.' });
-            }
-        } else {
-            return res.status(404).json({ exists: false, message: 'Email not found' });
-        }
-    } catch (error) {
-        winston.error(`Error in verifyEmailController for mail: ${mail} - ${error.message}`, {
-            stack: error.stack,
-            route: 'verifyEmail'
-        });
-    
-        if (error.name === 'SequelizeConnectionError') {
-            return res.status(500).json({ error: 'Database connection error. Please check the database configuration.' });
-        } else if (error.name === 'SequelizeValidationError') {
-            return res.status(400).json({ error: 'Invalid data input.' });
-        } else {
-            return res.status(500).json({ error: `Unexpected error: ${error.message}` });
-        }
-    }
-    
+    return res.status(400).json({ error: 'Email is required' });
+      
 };
 
 module.exports = verifyEmail;
