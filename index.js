4f3030c2a217962210d29e2ad521d5f22b395747
Williams Abiola <williams.abiola@itech.ng>
2024-11-19 09:16:26 +0100

    Improved Sequelize QUeries

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 
     if (!authenticate(req)) {
         logger.warn(`Unauthorized access attempt on ${req.path}`);
-        return res.status(401).json({ error: 'Invalid API Key, Version 1.0.2' });
+        return res.status(401).json({ error: 'Invalid API Key, Version 1.0.3' });
     }
     next();
 });
@@ -92,7 +92,7 @@ app.use('/fetchDividend', fetchDividendRoute);
 
 // Default Route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Estock Backend API 1.0.2');
+    res.send('Welcome to the Estock Backend API 1.0.3');
 });
 
 // Error Handling for Undefined Routes
